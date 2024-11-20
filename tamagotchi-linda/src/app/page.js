ba8cdbbd0f111e0c65fa5eca72a430e7f259132a
mialcheng <miacheng@gmail.com>
2024-11-20 17:04:51 -0600

    src/app/page.js mod

--- tamagotchi-linda/src/app/page.js
+++ tamagotchi-linda/src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center mb-6">Welcome to Our Site</h1>
       
       <div className="space-x-4">
-        <button className="px-6 py-2 bg-[#8F99FB] text-[#F8F7FF] rounded hover:bg-[#5A6DFF]">Create User</button>
-        <button className="px-6 py-2 bg-[#89CFF0] text-[#F8F7FF] rounded hover:bg-[#6EC6FF]">Login</button>
+        <button className="px-6 py-2 bg-[#8F99FB-500 text-[#F8F7FF] rounded hover:bg-blue-600">Create User</button>
+        <button className="px-6 py-2 bg-green-500 text-[#F8F7FF] rounded hover:bg-green-600">Login</button>
       </div>
     </div>
   );
