f7fd5c0bb312c6f9a6957ca67a8442816a655d5c
mialcheng <miacheng@gmail.com>
2024-11-20 16:58:49 -0600

    initial home page

--- tamagotchi-linda/src/Components/UserCard.jsx
+++ tamagotchi-linda/src/Components/UserCard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function UserCard() {
-  return <h1>MIA</h1>;
-}
+const UserCard = ({ user }) => {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/user/${user.id}`); 
+  };
+
+  return (
+    <div
+      className="border rounded p-4 shadow cursor-pointer"
+      onClick={handleCardClick}
+    >
+      <h2 className="text-xl font-bold">{user.name}</h2>
+    </div>
+  );
+};
+
+export default UserCard;
