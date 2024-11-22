14043980793586a9b97b26db7150d4111b5d5a24
AnnaRoseJones <AnnaRoseJones2025@u.northwestern.edu>
2024-11-22 15:02:52 -0600

    matching remote and local branch

--- tamagotchi-linda/src/app/page.js
+++ tamagotchi-linda/src/app/page.js
@@ -1,51 +1,28 @@
-"use client"
 import React from "react";
-import { useRouter } from 'next/navigation';
-import Link from 'next/link';
 import StatsPanel from "../components/StatsPanel"; // Adjust the path as needed
 import Image from "next/image";
 import PetIcon from "@/Components/PetIcon";
-import PetCard from "@/Components/PetCard";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleLoginClick = () => {
-    router.push("/loginPage");
-  };
-
-  const handleCreateAccountClick = () => {
-    router.push("/createAccount");
-  };
-  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-center mb-6 text-black">
+      <h1 className="text-4xl font-bold text-center mb-6 text-darkblue">
         Welcome to Our Site
       </h1>
       <div className="space-x-4">
-        <button 
-          className="px-6 py-2 bg-periwinkle text-white rounded hover:scale-110"
-          onClick={handleCreateAccountClick}
-        >
+        <button className="px-6 py-2 bg-periwinkle text-white rounded hover:bg-hoverperiwinkle">
           Create User
         </button>
-        <button
-          className="px-6 py-2 bg-lightblue text-white rounded hover:hover:scale-110"
-          onClick={handleLoginClick}
-        >
+        <button className="px-6 py-2 bg-lightblue text-white rounded hover:bg-hoverlightblue">
           Login
         </button>
       </div>
-
       {/* <PetIcon /> */}
 
       {/* Test panel component */}
       {/* <div className="w-full max-w-md">
         <StatsPanel />
       </div> */}
-
-      {/* <PetCard/> */}
     </div>
   );
-600}
+}
