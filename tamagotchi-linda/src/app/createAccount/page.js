8b3cea0c2628040185c4bb74e59ecfe93c076a6e
ShreyaSridhar5 <92066405+ShreyaSridhar5@users.noreply.github.com>
2024-11-29 20:37:09 -0500

    fixed import path error

--- tamagotchi-linda/src/app/createAccount/page.js
+++ tamagotchi-linda/src/app/createAccount/page.js
@@ -25,7 +25,6 @@ export default function CreateAccount() {
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
         <h1 className="text-2xl font-bold mb-6 text-center">Create Account</h1>
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
             <label
               htmlFor="username"
               className="block text-sm font-medium text-gray-700"
@@ -39,8 +38,7 @@ export default function CreateAccount() {
               onChange={(e) => setUsername(e.target.value)}
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               required
-            />
-          </div>
+            />          
           <button
             type="submit"
             className="w-full bg-darkblue text-white rounded-md py-2 px-2 hover:scale-110"
